0584152f399e872f16df3596883915bd87d9e079
iagooteles <iagooteles@gmail.com>
2025-04-01 01:10:23 -0300

    several changes on styles, specially dropdown, sticky navbar, etc

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const dropdownItems = document.querySelectorAll(".dropdown-item");
+  const categories = document.querySelectorAll(".mega-menu-categories a");
+  const submenus = document.querySelectorAll(".mega-menu-submenu");
+  const menu = document.querySelector(".mega-menu");
 
-    dropdownItems.forEach(item => {
-        item.addEventListener("mouseenter", function () {
-            document.querySelectorAll(".submenu").forEach(sub => {
-                sub.classList.add("d-none");
-            });
+  const activeSubmenu = menu.getAttribute("data-submenu-active");
+  const activeCategory = document.querySelector(
+    `.mega-menu-categories a[data-submenu-id="${activeSubmenu}"]`
+  );
 
-            const submenuId = item.getAttribute("data-submenu-id");
-            const submenu = document.getElementById(`submenu-${submenuId}`);
-            if (submenu) {
-                submenu.classList.remove("d-none");
-            }
-        });
+  if (activeCategory) {
+    activeCategory.classList.add("active");
+  }
 
-        item.addEventListener("mouseleave", function () {
-            const submenuId = item.getAttribute("data-submenu-id");
-            const submenu = document.getElementById(`submenu-${submenuId}`);
-            if (submenu) {
-                submenu.classList.add("d-none");
-            }
-        });
+  document
+    .getElementById(`submenu-${activeSubmenu}`)
+    .classList.remove("d-none");
+
+  categories.forEach((category) => {
+    category.addEventListener("mouseenter", function () {
+      const submenuId = this.getAttribute("data-submenu-id");
+
+      // Remover a classe ativa de todos os links e adicionar no link selecionado
+      categories.forEach((cat) => cat.classList.remove("active"));
+      this.classList.add("active");
+
+      submenus.forEach((submenu) => submenu.classList.add("d-none"));
+      document
+        .getElementById(`submenu-${submenuId}`)
+        .classList.remove("d-none");
     });
+  });
 });
