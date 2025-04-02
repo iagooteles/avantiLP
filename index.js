48c1519bd684cb494ea4923db764aaf86c7c5252
iagooteles <iagooteles@gmail.com>
2025-04-02 14:55:50 -0300

    added assets / finished footer

--- index.js
+++ index.js
@@ -31,3 +31,14 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+// Comentar este código depois //
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelectorAll(".footer-toggle").forEach(button => {
+    button.addEventListener("click", function () {
+      let arrow = this.querySelector(".arrow");
+      arrow.style.transform = this.classList.contains("collapsed") ? "rotate(0deg)" : "rotate(180deg)";
+    });
+  });
+});
